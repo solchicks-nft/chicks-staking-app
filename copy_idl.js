996ed2543968f29f59fc7495eac10920f19629f5
Jeansy Molanza <jeansymolanza@hotmail.co.uk>
2022-01-12 20:16:43 +0000

    Delete copy_idl.js

--- copy_idl.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires,import/no-unresolved */
-const fs = require('fs');
-const idl = require('./target/idl/chick_bridge.json');
-
-fs.writeFileSync('./app/src/idl.json', JSON.stringify(idl, null, 2));
